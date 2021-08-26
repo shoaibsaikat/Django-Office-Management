d99fc2fd4eb47d58ba04276d20ddbaa435552505
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-26 15:31:22 +0600

    login form bootstrap support

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from django.contrib.auth.models import User
 
 class SigninForm(forms.ModelForm):
+    username = forms.CharField(
+        help_text="Enter your username",
+        required=True,)
+    password = forms.CharField(
+        help_text="Enter your password",
+        required=True,
+        widget=forms.PasswordInput(),)
     class Meta:
         model = User
         fields = ['username', 'password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from .forms import SigninForm
 
 def signin(request):
     if request.method == 'POST':
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-        # form = forms.SigninForm(request.POST)
+        form = SigninForm(request.POST)
         if user is not None:
             login(request, user)
             return redirect('index')
-        else:
-            form = forms.SigninForm()
     else:
-        form = forms.SigninForm()
-
+        form = SigninForm()
     return render(request, 'accounts/signin.html', {'form': form})
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('index')
