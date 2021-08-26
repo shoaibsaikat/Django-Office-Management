55ea8e3aad62f9e03dc00e45162372b2ad7f9f23
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-26 10:37:44 +0600

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+
+# from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    # path('signup/', views.UserSignUpView.as_view(), name='signup'),
+]
---
+++ inventory/urls.py
@@ -0,0 +1,12 @@
+
+# from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+
+# from . import views
+
+app_name = 'inventory'
+
+urlpatterns = [
+    # path('signup/', views.UserSignUpView.as_view(), name='signup'),
+]
