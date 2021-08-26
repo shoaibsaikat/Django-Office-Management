94fb7ba0e159de335bc7f8316dac55ebb1d43167
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-26 16:09:34 +0600

    validation error shown

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 class SigninForm(forms.ModelForm):
@@ -12,3 +13,9 @@ class SigninForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+    
+    def clean(self):
+        cd = self.cleaned_data
+        user = authenticate(username=cd.get('username'), password=cd.get('password'))
+        if user is None:
+            raise forms.ValidationError("wrong username or password")
