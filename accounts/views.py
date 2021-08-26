0cec93f0cd484c74d531c9a57c220f2f209111b6
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-26 12:20:54 +0600

    login bug fix

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 def signin(request):
     if request.method == 'POST':
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is not None:
+            login(request, user)
             return redirect('index')
         else:
             form = forms.SigninForm()
