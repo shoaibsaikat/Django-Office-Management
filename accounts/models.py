6ed7174bdb3274da3ba9503bb88fad0fd226e541
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-08-29 14:16:23 +0600

    User model extension by One To One basis

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
+    supervisor = models.ForeignKey(User, on_delete=CASCADE, related_name='subordinates')
+
+    def __str__(self):
+        return str(self.user)
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
