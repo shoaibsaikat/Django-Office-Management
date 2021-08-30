9d4cc9db4acaa87f00778272bdc07a0bd13ecab4
Mina Shoaib Rahman <shoaibsaiakt@gmail.com>
2021-08-30 09:18:58 +0600

    inventory description update

--- inventory/models.py
+++ inventory/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Inventory(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
+    description = models.TextField(null=True)
     unit = models.CharField(max_length=255, default='item')
     count = models.IntegerField()
 
