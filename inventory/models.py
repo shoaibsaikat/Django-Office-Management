3c4541b491f14c9a797a276acc756b9c74112bd9
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2021-09-08 12:06:43 +0600

    inventory unit default given

--- inventory/models.py
+++ inventory/models.py
@@ -6,7 +6,7 @@ class Inventory(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
     unit = models.CharField(max_length=255, default='unit')
-    count = models.IntegerField()
+    count = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.name
@@ -16,6 +16,8 @@ class Requisition(models.Model):
     user = models.ForeignKey(User, on_delete=CASCADE, related_name='requests')
     approver = models.ForeignKey(User, on_delete=CASCADE, related_name='requested_items')
     approved = models.BooleanField(null=True, blank=True, default=False)
+    # distributor = models.ForeignKey(User, on_delete=CASCADE, blank=True, null=True, related_name='approved_items')
+    # distributed = models.BooleanField(null=True, blank=True, default=False)
     title = models.CharField(max_length=255)
     amount = models.IntegerField()
     comment = models.TextField(null=True, blank=True)
